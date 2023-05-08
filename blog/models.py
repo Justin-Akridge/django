59a55b2f458095ab1503acd95c300f325469ac54
Justin Akridge <Akridjus@gmail.com>
2023-05-07 20:07:57 -0400

    Forms, Data, and Security

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 from django.conf import settings
 from django.db import models
@@ -19,3 +20,20 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
+        
+def approved_comments(self):
+    return self.comments.filter(approved_comment=True)
